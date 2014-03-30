f3e62a2a26ff7be0d73f061c9d0b444604db8bec
Dominic Muller <nicklink483@gmail.com>
2014-03-30 02:32:59 -0700

    moved over win32-sound for use until functionality is added to original gem

--- lib/juicy.rb
+++ lib/juicy.rb
@@ -1,18 +1,18 @@
 # require 'win32-sound'
 # require_relative 'juicy/win32/sound.rb'
-require_relative '../../win32-sound/lib/win32/sound.rb'
+require_relative 'win32/sound'
 
-require_relative 'juicy/pitch.rb' #toned (or not) frequency in a chosen pitch space (temperament/intonation)
-  require_relative 'juicy/note.rb' #named pitch
-  require_relative 'juicy/duration.rb' #length of note in musical time (quarter note, eighth note, etc.)
-    require_relative 'juicy/chord.rb' #collection of notes
-      require_relative 'juicy/chord_progression.rb' #collection of chords in sequence
-      require_relative 'juicy/melody.rb' #collection of chords in sequence
+require_relative 'juicy/pitch' #toned (or not) frequency in a chosen pitch space (temperament/intonation)
+  require_relative 'juicy/note' #named pitch
+  require_relative 'juicy/duration' #length of note in musical time (quarter note, eighth note, etc.)
+    require_relative 'juicy/chord' #collection of notes
+      require_relative 'juicy/chord_progression' #collection of chords in sequence
+      require_relative 'juicy/melody' #collection of chords in sequence
       
-    require_relative 'juicy/scale.rb' #sequence of relative pitch changes ex. chromatic, diatonic, whole-note, pentatonic
-  require_relative 'juicy/mode.rb' #'flavor' of diatonic scale
-  require_relative 'juicy/scale_degree.rb' #index of given scale relative to root/tonic
-  require_relative 'juicy/key.rb' #scale at a given root note, i.e. Juicy::Note
-  require_relative 'juicy/voice.rb' #an instrument
-  require_relative 'juicy/track.rb' #a track of notes
-  require_relative 'juicy/song.rb' #
+    require_relative 'juicy/scale' #sequence of relative pitch changes ex. chromatic, diatonic, whole-note, pentatonic
+  require_relative 'juicy/mode' #'flavor' of diatonic scale
+  require_relative 'juicy/scale_degree' #index of given scale relative to root/tonic
+  require_relative 'juicy/key' #scale at a given root note, i.e. Juicy::Note
+  require_relative 'juicy/voice' #an instrument
+  require_relative 'juicy/track' #a track of notes
+  require_relative 'juicy/song' #
