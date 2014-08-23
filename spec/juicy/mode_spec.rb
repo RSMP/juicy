c6159aeab19a4bc21914aab98ebbd5171a00644e
Dominic Muller <nicklink483@gmail.com>
2014-08-22 22:01:20 -0700

    add == specs to Juicy::Mode

--- spec/juicy/mode_spec.rb
+++ spec/juicy/mode_spec.rb
@@ -6,4 +6,20 @@ describe Juicy::Mode do
       expect(Juicy::Mode.new.type).to eq :ionian
     end
   end
+  describe "#==" do
+    context "the modes are the same" do
+      it "should return true" do
+        mode1 = Mode.new(type: :major)
+        mode2 = Mode.new(type: :major)
+        expect(mode1 == mode2).to eq true
+      end
+    end
+    context "the modes are different" do
+      it "should return false" do
+        mode1 = Mode.new(type: :major)
+        mode2 = Mode.new(type: :minor)
+        expect(mode1 == mode2).to eq false
+      end
+    end
+  end
 end
